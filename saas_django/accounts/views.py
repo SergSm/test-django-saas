a42cc4393746a51efd055b613240cb6a08b7c807
SergeyS <smirnovserg.s@gmail.com>
2020-08-30 14:09:02 +0600

    accounts/views.py

--- saas_django/accounts/views.py
+++ saas_django/accounts/views.py
@@ -1,5 +1,9 @@
+from rest_framework import permissions
+from rest_framework import generics
 from django.contrib.auth import get_user_model
-from rest_framework import generics, permissions
+
+# new imports
+from core.views import CompanySageViewMixin
 from . import serializers
 
 
@@ -18,21 +22,14 @@ class UserList(generics.ListCreateAPIView):
     permission_classes = (
         permissions.IsAuthenticated,
     )
+
     serializer_class = serializers.UserSerializer
     queryset = User.objects.all()
 
-    def perform_create(self, serializer):
-        company_id = self.request.user.company_id
-        serializer.save(company_id=company_id)
-
-    # Overrides the default method
-    def get_queryset(self):
-        # !!!Ensure that the users belong to the company of the user
-        # that is making the request
-        company_id = self.request.user.company_id
-        return super().get_queryset().filter(company_id=company_id)
+    # removed get_queryset and perform_create
 
 
+# Added CompanySafeViewMixin
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     name = 'user-detail'
     permission_classes = (
@@ -42,12 +39,7 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.UserSerializer
     queryset = User.objects.all()
 
-    def get_queryset(self):
-        # Ensure that the user belongs to the company of the user
-        # that is making the request
-        # Note that this method is identical to the one in `UserList`
-        company_id = self.request.user.company_id
-        return super().get_queryset().filter(company_id=company_id)
+    # Removed get_queryset
 
 
 class CompanyDetail(generics.RetrieveUpdateAPIView):
