d750379828ea16c66ae69d227ccb08794fdca474
SergeyS <smirnovserg.s@gmail.com>
2020-08-30 14:03:27 +0600

    core/views.py

--- saas_django/core/views.py
+++ saas_django/core/views.py
@@ -8,6 +8,15 @@ class CompanySafeViewMixin:
     and are querysets are filtered for read operations
     """
     def get_query(self):
-        quyeryset = super().get_queryset(
+        queryset = super().get_queryset()
 
         if not self.request.user.is_authenticated:
+            raise exceptions.NotAuthenticated()
+
+        company_id = self.request.user.company_id
+        return queryset.filter(company_id=company_id)
+
+    def perform_create(self, serializer):
+        company_id = self.request.user.company_id
+        serializer.save(company_id=company_id)
+
